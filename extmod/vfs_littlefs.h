9c5a5db50f0b9b23cd5ca337e847516f7d6d91dc
Stephen Irons <ironss@users.noreply.github.com>
2019-08-02 15:33:57 +1200

    Implemented dummy functions for LittleFS VFS, based on FatFS implementation.

--- extmod/vfs_littlefs.h
+++ extmod/vfs_littlefs.h
@@ -23,13 +23,13 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
- 
 
 #ifndef MICROPY_INCLUDED_EXTMOD_VFS_LITTLEFS_H
 #define MICROPY_INCLUDED_EXTMOD_VFS_LITTLEFS_H
 
 #include "py/obj.h"
 extern const mp_obj_type_t mp_littlefs_vfs_type;
+//MP_DECLARE_CONST_FUN_OBJ_3(littlefs_vfs_open_obj);
 
 #endif
 
